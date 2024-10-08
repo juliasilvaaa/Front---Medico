a7340f8704ec6f6bf62a873f17e7a9ad0f202440
JULIA PEREIRA SILVA <23111810@CFP127ALUNO.LOCAL>
2024-10-08 12:07:55 -0300

    comecando

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,56 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+
+
+export default function Login() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    // Imagem do Médico
+    <div className="flex h-screen">
+      <div className="w-1/2 bg-cover bg-center bg-medico-padrao">
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      {/* Div para textos, inputs e botões */}
+      <div className="w-1/2 flex flex-col justify-center items-center p-8">
+        <h1 className="text-3xl font-bold mb-4 text-[--azulprincipal] font-poppins">Login</h1>
+
+
+
+       {/* Email */}
+        <form className="w-[40vh]">
+          <div className="mb-4 relative">
+            <label htmlFor="email" className="block mb-2">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="border border-gray-300 rounded p-2 pl-10 w-full"
+              placeholder="Seu email"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <img src="/img/iconemail.png" alt="Ícone de email" className=" transform -translate-y-1/2 w-5 h-5" />
+          </div>
+
+
+        {/* Senha  */}
+          <div className="mb-4 relative">
+            <label htmlFor="password" className="block mb-2">Senha</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="border border-gray-300 rounded p-2 pl-10 w-full"
+              placeholder="Sua senha"
+            />
+            <img src="/img/ocultarsenha.png" alt="Ícone de cadeado" className="transform -translate-y-1/2 w-5 h-5" />
+          </div>
+
+          <button type="submit" className="bg-[--azulprincipal] text-white rounded-lg p-2 w-full">Entrar</button>
+        </form>
+
+        <p className="mt-4 text-center">Esqueceu sua senha? <a href="/reset-password" className="text-blue-500">Clique aqui</a></p>
+        <p className="text-center">Não tem uma conta? <a href="/register" className="text-blue-500">Registre-se agora</a>.</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'medico-padrao': 'var(--medico-padrao-branco-cis)'
+      }
     },
   },
   plugins: [],
